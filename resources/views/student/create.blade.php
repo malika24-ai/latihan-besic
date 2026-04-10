de2cd06daad1f773baecbdf85ac64998c0d7fe71
lika <malika24@mhs.unitama.ac.id>
2026-04-11 01:32:44 +0700

    pertemuan 2: Blade Template

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+  <x-slot:title>{{ $title }}</x-slot>
+
+
+    <h1.fw-bold>From create student</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/student', function () {
+    return view('student.index', ['title' => 'Student']);
+});
+Route::get('/student/create', function () {
+    return view('student.create', ['title' => 'create Student']);
+});
